505f6182d5dfcaa3133e0828f1ee274776c1a9b3
digitalaltar <moon@digitalaltar.com>
2024-09-26 13:03:36 -0700

    Taking a new direction with the foundation

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 const path = require('path');
 
 module.exports = {
-  entry: './source.js',  // Entry point (your unbundled JavaScript file)
+  entry: './source.js',
   output: {
-    filename: 'app.js',  // Output file (Webpack will generate this)
-    path: path.resolve(__dirname, 'public'),  // Output directory set to /public
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'public'),
   },
-  mode: 'development',  // Use 'development' for debugging and 'production' for optimized builds
+  resolve: {
+      extensions: ['.js']
+  },
+  mode: 'development',
 };
